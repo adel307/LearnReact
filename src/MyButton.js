17b4637b73c9c86aab0ef3ad99870de55375e78c
adel307 <adel.307.ahmed@gmail.com>
2025-12-27 07:07:29 +0200

    Style MyButtom add shadow

--- src/MyButton.js
+++ src/MyButton.js
@@ -1,6 +1,7 @@
 let content = {
   name : "adel",
   number : "1",
+  content : "my button",
 }
 
 let SayHallo = function () {
@@ -10,12 +11,13 @@ let SayHallo = function () {
 let MyButtonStyle = {
   backgroundColor : "red",
   fontSize : "20px",
+  boxShadow : "10px 10px 10px black",
 }
 
 function MyButton () {
   return (
     <>
-      <button style={MyButtonStyle} onClick={SayHallo} > {content.number} button</button>
+      <button style={MyButtonStyle} onClick={SayHallo} > {content.content} </button>
     </>
   );
 }
