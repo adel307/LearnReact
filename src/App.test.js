bca62ae398b8531e712ba7b6f67bf6da75489ff8
adel307 <adel.307.ahmed@gmail.com>
2026-01-05 15:50:13 +0200

    react project 3

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders react_project3 link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/react project3/i);
   expect(linkElement).toBeInTheDocument();
 });
